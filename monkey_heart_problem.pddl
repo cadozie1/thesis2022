2318fc2f286915339bb5e9f2664b88d0d4d6a153
cadozie1 <cadozie1@asu.edu>
2022-10-01 13:41:58 -0700

    added third story and updated input to gpt3

--- monkey_heart_problem.pddl
+++ monkey_heart_problem.pddl
@@ -1,11 +1,11 @@
 (define (problem monkey_heart) (:domain monkey_heart_story)
 (:objects 
-monkey - animal
-shark - animal
-sultan - animal
-lion - animal
-donkey - animal
-hare - animal
+the_monkey - animal
+the_shark - animal
+the_sultan - animal
+the_lion - animal
+the_donkey - animal
+the_hare - animal
 cliff - location
 cave - location
 tree - location
@@ -14,14 +14,14 @@ forest - location
 stream - location
 )
 (:init
-    (in monkey tree)
-    (in shark sea)
-    (in sultan sea)
-    (in lion cave)
-    (in hare cave)
-    (in donkey forest)
-    (sick sultan)
-    (sick lion)
+    (in the_monkey tree)
+    (in the_shark sea)
+    (in the_sultan sea)
+    (in the_lion cave)
+    (in the_hare cave)
+    (in the_donkey forest)
+    (sick the_sultan)
+    (sick the_lion)
     (connected tree cliff)
     (connected cave forest)
     (connected forest stream)
@@ -31,15 +31,15 @@ stream - location
     (connected forest cave)
     (connected cliff tree)
 
-    (foolish donkey)
-    (want_heart hare)
+    (foolish the_donkey)
+    (want_heart the_hare)
 
 )
 (:goal (and
-    (tricked_twice donkey)
-    (tricked_once monkey)
-    (tricked_once shark)
-    (heart_eater lion)
-    (not (heart_eater sultan))
+    (tricked_twice the_donkey)
+    (tricked_once the_monkey)
+    (tricked_once the_shark)
+    (heart_eater the_lion)
+    (not (heart_eater the_sultan))
 ))
 )
--- plan_to_text.py
+++ plan_to_text.py
@@ -7,47 +7,57 @@ def actionToText(action, storyNum):
     action_name = action[0]
     if storyNum == 0:
         if action_name == "climb_tower":
-            return action[1].capitalize() + " climbs the tower with the help of " + action[2] + ". "
+            return action[1].capitalize() + " climbs the tower with the help of " + action[2] + ".\n"
         elif action_name == "walk":
-            return action[1].capitalize() + " walks from the " + action[2] + " to the " + action[3] + ". "
+            return action[1].capitalize() + " walks from the " + action[2] + " to the " + action[3] + ".\n"
         elif action_name == "grow_hair":
-            return "After years of waiting, " + action[1] + "'s hair grows out. "
+            return "After years of waiting, " + action[1] + "'s hair grows out.\n"
         elif action_name == "descend_tower":
-            return action[1].capitalize()+ " descends the tower with the help of " + action[2] + ". "
+            return action[1].capitalize()+ " descends the tower with the help of " + action[2] + ".\n"
         elif action_name == "jump_down_tower":
-            return action[1].capitalize() + " has no way down, so " + action[1] + " jumps off the tower. "
+            return action[1].capitalize() + " has no way down, so " + action[1] + " jumps off the tower.\n"
         elif action_name == "climb_up_ladder":
-            return action[1].capitalize() + " uses the ladder to climb up the tower. "
+            return action[1].capitalize() + " uses the ladder to climb up the tower.\n"
         elif action_name == "climb_up_hair":
-            return action[1].capitalize() + " uses the cut hair to climb up the tower. "
+            return action[1].capitalize() + " uses the cut hair to climb up the tower.\n"
         elif action_name == "climb_down_ladder":
-            return action[1].capitalize() + " uses the ladder to climb down the ladder. "
+            return action[1].capitalize() + " uses the ladder to climb down the ladder.\n"
         elif action_name == "climb_down_hair":
-            return action[1].capitalize() + " uses the cut hair to climb down the tower. "
+            return action[1].capitalize() + " uses the cut hair to climb down the tower.\n"
         elif action_name == "serenade":
-            return action[1].capitalize() + " sings, serenading everyone around. "
+            return action[1].capitalize() + " sings, serenading everyone around.\n"
         elif action_name == "sweet_talk":
-            return action[1].capitalize() + " sweet talks " + action[2] + ". "
+            return action[1].capitalize() + " sweet talks " + action[2] + ".\n"
         elif action_name == "cut_hair":
-            return action[1].capitalize() + " cuts " + action[2] + "'s hair. "
+            return action[1].capitalize() + " cuts " + action[2] + "'s hair.\n"
         elif action_name == "marry":
-            return action[1].capitalize() + " marries " + action[2] + ". "
+            return action[1].capitalize() + " marries " + action[2] + ".\n"
         elif action_name == "give_ladder":
-            return action[1].capitalize() + " gives the ladder to " + action[2] + ". "
+            return action[1].capitalize() + " gives the ladder to " + action[2] + ".\n"
 
     elif storyNum == 1:
         if action_name == "walk":
-            return action[1].capitalize() + " walks from the " + action[2] + " to the " + action[3] + ", and all friends of " + action[1] + " that are awake nearby follow. "
+            return action[1].capitalize() + " walks from the " + action[2] + " to the " + action[3] + ", and all friends of " + action[1] + " that are awake nearby follow.\n"
         elif action_name == "rest":
-            return action[1].capitalize() + " rests in the " + action[2] + ". "
+            return action[1].capitalize() + " rests in the " + action[2] + ".\n"
         elif action_name == "talk":
-            return action[1].capitalize() + " talks to " + action[2] + ". "
+            return action[1].capitalize() + " talks to " + action[2] + ".\n"
         elif action_name == "pick_up":
-            return action[1].capitalize() + " picks up the " + action[2] + " in the " + action[3] + ". "
+            return action[1].capitalize() + " picks up the " + action[2] + " in the " + action[3] + ".\n"
         elif action_name == "give":
-            return action[1].capitalize() + " gives the " + action[3] + " to " + action[2] + ". "
+            return action[1].capitalize() + " gives the " + action[3] + " to " + action[2] + ".\n"
         elif action_name == "drop":
-            return action[1].capitalize() + " drops the " + action[2] + " in the " + action[3] + ". "
+            return action[1].capitalize() + " drops the " + action[2] + " in the " + action[3] + ".\n"
+    elif storyNum == 2:
+        if action_name == "move":
+            return action[1] + " moves from the " + action[2] + " to the " + action[3] + ", accompanied by anyone travelling with " + action[1] + ".\n"
+        elif action_name == "trick":
+            return action[1] + " tricks " + action[2] + ".\n"
+        elif action_name == "move_together":
+            return action[1] + " and " + action[2] + " start travelling together.\n"
+        elif action_name == "eat_heart":
+            return action[1] + " eats " + action[2] + "'s heart since " + action[2] + " was tricked twice.\n"
+
     #else:
 
 def planFileToActions(planfile):
@@ -69,7 +79,7 @@ def do_everything(initial_prompt, domain, problem, plan, gpt3_r, gpt3_w, storyNu
     f = open(gpt3_r, "w")
     with open(initial_prompt) as prompt:
         f.write(prompt.read())
-    f.write("\nHere is the sequence of actions that the story should follow:\n\n")
+    #f.write("\nHere is the sequence of actions that the story should follow:\n\n")
     f.close()
     #gpt3.complete(gpt3_r, gpt3_r, "text-davinci-002", 40, 0.7) 
 #    f = open(gpt3_r, "w")
@@ -81,23 +91,30 @@ def do_everything(initial_prompt, domain, problem, plan, gpt3_r, gpt3_w, storyNu
     #f = open(gpt3_w, "w")
     #f.close()
     for action in planFileToActions(plan):
-        f = open(gpt3_r, "a")
+        f = open(gpt3_r, "w")
+        f.write("\nHere is the next action in the story:\n")
         action_text = actionToText(action, storyNum)
         f.write(action_text)
         f.close()
 
-    f = open(gpt3_r, "a")
-    f.write("\n\nParaphrase the story with as few words from the original text as possible:\n\n")
-    f.close()
+        f = open(gpt3_r, "a")
+        f.write("\n\nParaphrase that action in different, more elaborate words:\n")
+        f.close()
 
-    gpt3.complete(gpt3_r, gpt3_w, "text-davinci-002", 256, 0.9)
+        gpt3.complete(gpt3_r, gpt3_w, "text-davinci-002", 256, 0.9)
+        #f = open(gpt3_r, "a")
+        #with open(gpt3_r, "r") as write_text:
+        #    f.write(action_text)
+        #f.close()
+
+
         #f = open(gpt3_w, "a")
         #f.write(action_text)
         #f.close()
 
         #gpt3.complete(gpt3_r, gpt3_w, "text-davinci-002", 40, 0.7)
         #f = open(gpt3_w, "a")
-        #f.write(". ")
+        #f.write(".\n")
         #f.close()
 
         #f = open(gpt3_r, "a")
@@ -110,10 +127,11 @@ def do_everything(initial_prompt, domain, problem, plan, gpt3_r, gpt3_w, storyNu
         
 
 def main():
-    do_everything("rapunzel_prompt.txt", "rapunzel_domain.pddl", "rapunzel_problem.pddl", "rapunzel_plan.ipc", "rapunzel_gpt3_read.txt", "rapunzel_gpt3_write.txt", 0)
-#    do_everything("golden_nugget_prompt.txt", "golden_nugget_domain.pddl", "golden_nugget_problem.pddl", "golden_nugget_plan.ipc", "golden_nugget_gpt3_read.txt", "golden_nugget_gpt3_write.txt", 1)
+    #do_everything("rapunzel_prompt.txt", "rapunzel_domain.pddl", "rapunzel_problem.pddl", "rapunzel_plan.ipc", "rapunzel_gpt3_read.txt", "rapunzel_gpt3_write.txt", 0)
+#    do_everything("golden_nugget_prompt.txt".\n"golden_nugget_domain.pddl".\n"golden_nugget_problem.pddl".\n"golden_nugget_plan.ipc".\n"golden_nugget_gpt3_read.txt".\n"golden_nugget_gpt3_write.txt", 1)
+    do_everything("monkey_heart_prompt.txt", "monkey_heart_domain.pddl", "monkey_heart_problem.pddl", "monkey_heart_plan.ipc", "monkey_heart_gpt3_read.txt", "monkey_heart_gpt3_write.txt", 2)
 
-#    solve.find_plan("golden_nugget_domain.pddl", "golden_nugget_problem.pddl", "golden_nugget_plan.ipc")
+#    solve.find_plan("golden_nugget_domain.pddl".\n"golden_nugget_problem.pddl".\n"golden_nugget_plan.ipc")
 #    for action in planFileToActions("golden_nugget_plan.ipc"):
 #        print(actionToText(action, 1))
 
