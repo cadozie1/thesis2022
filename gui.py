2f36683ea0c9b7ac7c7a03f6d10a815e34832792
cadozie1 <cadozie1@asu.edu>
2022-10-16 16:51:20 -0700

    removed unneeded files

--- gui.py
+++
@@ -1,6 +0,0 @@
-import tkinter
-
-m=tkinter.Tk()
-w = Button(master, command=
-
-m.mainloop()
--- new.py
+++
@@ -1,6 +0,0 @@
-import solve
-
-def main():
-    solve.find_plan("golden_nugget_domain.pddl", "golden_nugget_problem.pddl", "golden_nugget_plan.ipc")    
-if __name__ == "__main__":
-    main()
