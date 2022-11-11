e2bdf871f2934cfb2849128451b3325ccde8df37
cadozie1 <cadozie1@asu.edu>
2022-11-10 21:40:26 -0700

    found error in golden_nugget 2 & 3 results

---
+++ new.py
@@ -0,0 +1,8 @@
+import solve
+
+def main():
+    
+    solve.find_plan("./golden_nugget2/pddl/domain.pddl", "./golden_nugget2/pddl/problem.pddl", "g2plan.ipc")
+
+if __name__ == "__main__":
+    main()
